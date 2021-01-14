93bc6806eeece0f4f642e7fae40fc93b7f98d9ef
karice <jiwon803@gmail.com>
2021-01-14 20:49:21 +0900

    삭제 기능 추가

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -45,4 +45,13 @@ public class UserController {
 
         return ResponseEntity.created(location).build();
     }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        User user = service.deleteById(id);
+
+        if (user == null){
+            throw new UserNotFoundException(String.format("id[%s] not found",id));
+        }
+    }
 }
--- src/main/java/com/example/restfulwebservice/user/UserDaoService.java
+++ src/main/java/com/example/restfulwebservice/user/UserDaoService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -43,5 +44,21 @@ public class UserDaoService {
             }
         }
         return null;
+    }
+
+    public User deleteById(int id){
+        Iterator<User> iterator = users.iterator();
+
+        while (iterator.hasNext()){
+            User user = iterator.next();
+
+            if (user.getId() == id){ // 있으면
+                iterator.remove();
+                return user;
+            }
+
+
+        }
+        return null; //없으면 Null 반환
     }
 }
