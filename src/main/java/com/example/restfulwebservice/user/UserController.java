42e5b6df0330d8fda3e68bdc12316d22aef8a167
karice <jiwon803@gmail.com>
2021-01-15 20:54:48 +0900

    이름은 2글자 validation check

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,10 +1,10 @@
 package com.example.restfulwebservice.user;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -35,7 +35,7 @@ public class UserController {
     }
 
     @PostMapping("/users") // 유저 추가해주는 것
-    public ResponseEntity<User> createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
         User savedUser = service.save(user);
 
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
